5e6d722d95044b581a37eb95032a40c3db7c3c85
Naina <71379702+na2004@users.noreply.github.com>
2020-10-21 01:01:41 +0400

    Added a boundary for the player icon

--- public/javascript/player.js
+++ public/javascript/player.js
@@ -3,21 +3,26 @@ function playerMovement(){
 }
 
 function move(e) {
+    var element = document.getElementById("icon");
+    if(element.style.left == '-180px')// boundaries are set
+        element.style.left = '-175px';
+    else if(element.style.left =='870px')
+        element.style.left = '865px';
+    else if(element.style.top =='-180px')
+        element.style.top = '-175px';
+    else if(element.style.top =='160px')
+        element.style.top = '155px';
     switch (e.keyCode) {
         case 65:
-            var element = document.getElementById("icon");
             element.style.left = parseInt(element.style.left) - 5 + 'px';
         break;
         case 68:
-            var element = document.getElementById("icon");
             element.style.left = parseInt(element.style.left) + 5 + 'px';
         break;
         case 87:
-            var element = document.getElementById("icon");
             element.style.top = parseInt(element.style.top) - 5 + 'px';
         break;
         case 83:
-            var element = document.getElementById("icon");
             element.style.top = parseInt(element.style.top) + 5 + 'px';
         break;
     }

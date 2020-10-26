4c3e4c99be6b56b9958d4e6a5b0138c09ac2cd77
na2004@hw.ac.uk <71379702+na2004@users.noreply.github.com>
2020-10-27 00:10:57 +0400

    Added chat function

--- public/javascript/player.js
+++ public/javascript/player.js
@@ -1,8 +1,58 @@
-function playerMovement(){
-    window.addEventListener('keydown',move);
-}
+var Key = {
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40
+  };
+  
+function listener(evt, element, fn) {
+    if (window.addEventListener) 
+        element.addEventListener(evt, fn, false);
+    else
+      element.attachEvent('on' + evt, fn);
+  }
+  
+  function move(evt) {
+    if (!evt)
+      evt = window.event;
+    var keycode = evt.keyCode || evt.which; 
+  
+    var info = document.getElementById("chat-list");
+    var blob = document.getElementById("icon");
+    switch (keycode) {
+        case Key.LEFT:
+          blob.style.left = parseInt(blob.style.left) - 5 + 'px';
+        break;
+        case Key.UP:
+          blob.style.top = parseInt(blob.style.top) - 5 + 'px';
+        break;
+        case Key.RIGHT:
+          blob.style.left = parseInt(blob.style.left) + 5 + 'px';
+        break;
+        case Key.DOWN:
+          blob.style.top = parseInt(blob.style.top) + 5 + 'px';
+        break;
+        case 32: // space button
+            var element = document.getElementById("icon");
+            var laser = document.getElementById("laser");
+            laser.style.top = parseInt(element.style.top) + 170 + 'px';
+            laser.style.left = parseInt(element.style.left) + 300 +'px';
+            playerShoot();
+        break;
+        default:
+            return;
+    }
+  }
+  
+ listener('keydown', document, move);
 
-function playerShoot(){
+  function message()//to display the chat text
+  {
+      var chat = document.getElementById("chat-input");
+      document.getElementById("chat-list").innerHTML = chat.value;
+  }
+
+  function playerShoot(){ // to shoot bullets
     let start = Date.now();
 
     let timer = setInterval(function() {
@@ -11,34 +61,4 @@ function playerShoot(){
     if (timePassed >= 2000){clearInterval(timer); return;}
     laser.style.top = parseInt(laser.style.top) -12 +'px';
 
-}, 20);
-}
-
-function move(e) {
-    switch (e.keyCode) {
-        case 65:
-            var element = document.getElementById("icon");
-            element.style.left = parseInt(element.style.left) - 5 + 'px';
-        break;
-        case 68:
-            var element = document.getElementById("icon");
-            element.style.left = parseInt(element.style.left) + 5 + 'px';
-        break;
-        case 87:
-            var element = document.getElementById("icon");
-            element.style.top = parseInt(element.style.top) - 5 + 'px';
-        break;
-        case 83:
-            var element = document.getElementById("icon");
-            element.style.top = parseInt(element.style.top) + 5 + 'px';
-        break;
-        case 32: // space button
-            var element = document.getElementById("icon");
-            var laser = document.getElementById("laser");
-            laser.style.top = parseInt(element.style.top) + 170 + 'px';
-            laser.style.left = parseInt(element.style.left) + 300 +'px';
-            playerShoot();
-
-    }
-    e.preventDefault();
-};
+}, 20);}

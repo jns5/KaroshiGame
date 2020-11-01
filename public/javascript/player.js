0c7fc61bc56a0631f2f8fe2ef13ea8df572ff66c
AloraTab <59279709+AloraTab@users.noreply.github.com>
2020-11-01 14:50:37 +0400

    Directional Shooting
    Can now shoot in the direction player is moving

--- public/javascript/player.js
+++ public/javascript/player.js
@@ -19,50 +19,126 @@ function listener(evt, element, fn) {
   
     var info = document.getElementById("chat-list");
     var blob = document.getElementById("icon");
+    // var direction = "Up";
     switch (keycode) {
         case Key.LEFT:
           blob.style.left = parseInt(blob.style.left) - 5 + 'px';
           movePointer(keycode);
+          direction = "Left";
         break;
         case Key.UP:
           blob.style.top = parseInt(blob.style.top) - 5 + 'px';
           movePointer(keycode);
+          direction = "Up";
         break;
         case Key.RIGHT:
           blob.style.left = parseInt(blob.style.left) + 5 + 'px';
           movePointer(keycode);
+          direction = "Right";
+
         break;
         case Key.DOWN:
           blob.style.top = parseInt(blob.style.top) + 5 + 'px';
           movePointer(keycode);
+          direction = "Down";
         break;
         case 32: // space button
-            var element = document.getElementById("icon");
-            var laser = document.getElementById("laser");
-            laser.style.top = parseInt(element.style.top) + 230 + 'px';
-            laser.style.left = parseInt(element.style.left) + 290 +'px';
-            playerShoot();
+            // var element = document.getElementById("icon");
+            // var laser = document.getElementById("laser");
+            // laser.style.top = parseInt(element.style.top) + 230 + 'px';
+            // laser.style.left = parseInt(element.style.left) + 290 +'px';
+            // playerShoot();
+            
+            switch (direction) {
+              case "Left":
+                playerShootLeft();
+              break;
+              case "Right":
+                playerShootRight();
+              break;
+              case "Up":
+                playerShootUp();
+              break;
+              case "Down":
+                playerShootDown();
+              break;
+            }
         break;
         default:
             return;
     }
 }
- listener('keydown', document, move);
+listener('keydown', document, move);
 
   function message()//to display the chat text
   {
       var chat = document.getElementById("chat-input");
       document.getElementById("chat-list").innerHTML = chat.value;
   }
-
-  function playerShoot(){ // to shoot bullets
+  
+  function playerShootUp(){ // to shoot bullets upwards
+    var element = document.getElementById("icon");
+    var laser = document.getElementById("laser");
+    laser.style.top = parseInt(element.style.top) + 230 + 'px';
+    laser.style.left = parseInt(element.style.left) + 290 +'px';
     let start = Date.now();
 
     let timer = setInterval(function() {
     let timePassed = Date.now() - start;
 
-    if (timePassed >= 2000){clearInterval(timer); return;}
+    if (timePassed >= 2000)
+    {clearInterval(timer); return;}
     laser.style.top = parseInt(laser.style.top) -8 +'px';
+
+  }, 20);}
+
+  function playerShootDown(){ // to shoot bullets downwards
+    var element = document.getElementById("icon");
+    var laser = document.getElementById("laser");
+    laser.style.top = parseInt(element.style.top) + 420 + 'px';
+    laser.style.left = parseInt(element.style.left) + 290 +'px';
+    let start = Date.now();
+
+    let timer = setInterval(function() {
+    let timePassed = Date.now() - start;
+
+    if (timePassed >= 2000)
+    {clearInterval(timer); return;}
+    laser.style.top = parseInt(laser.style.top) +8 +'px';
+
+  }, 20);}
+
+  function playerShootRight(){// to shoot bullets to the right
+    var element = document.getElementById("icon");
+    document.getElementById("laser").style.transform = "rotate(90deg)";
+    var laser = document.getElementById("laser");
+    laser.style.top = parseInt(element.style.top) + 300 + 'px';
+    laser.style.left = parseInt(element.style.left) + 410 +'px';
+    let start = Date.now();
+
+    let timer = setInterval(function() {
+    let timePassed = Date.now() - start;
+
+    if (timePassed >= 2000)
+    {clearInterval(timer); return;}
+    laser.style.left = parseInt(laser.style.left) +8 +'px';
+
+  }, 20);}
+
+  function playerShootLeft(){ // to shoot bullets to the left
+    var element = document.getElementById("icon");
+    document.getElementById("laser").style.transform = "rotate(90deg)";
+    var laser = document.getElementById("laser");
+    laser.style.top = parseInt(element.style.top) + 300 + 'px';
+    laser.style.left = parseInt(element.style.left) + 180 +'px';
+    let start = Date.now();
+
+    let timer = setInterval(function() {
+    let timePassed = Date.now() - start;
+
+    if (timePassed >= 2000)
+    {clearInterval(timer); return;}
+    laser.style.left = parseInt(laser.style.left) -8 +'px';
 
   }, 20);}
 

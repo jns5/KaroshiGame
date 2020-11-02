dd1a19a9c83865944bb0202e3ec4ca538da9c89d
AndreaNabua <acn2000@hw.ac.uk>
2020-11-02 12:15:58 +0400

    added username form on game page

---
+++ public/javascript/main.js
@@ -0,0 +1,7 @@
+let username = document.getElementById("username");
+let transparentOverlay = document.getElementsByClassName("transparent-overlay");
+let usernameFormContainer = document.getElementsByClassName("username-form");
+const submit = () => { 
+    transparentOverlay.style.display = "none";
+    usernameFormContainer.style.display = "none";
+};

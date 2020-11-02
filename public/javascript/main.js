4a886e3a054053dd9ab7c6f56b7d45765435db1a
mrt2000HW <71808733+mrt2000HW@users.noreply.github.com>
2020-11-02 18:49:08 +0400

    Username screen fixed

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,7 +1,1 @@
-let username = document.getElementById("username");
-let transparentOverlay = document.getElementsByClassName("transparent-overlay");
-let usernameFormContainer = document.getElementsByClassName("username-form");
-const submit = () => { 
-    transparentOverlay.style.display = "none";
-    usernameFormContainer.style.display = "none";
-};
+

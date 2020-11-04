09f4979466d4b27a3f038dad2880d14ebbea378e
johnmendez2 <66302608+johnmendez2@users.noreply.github.com>
2020-11-04 17:25:27 +0400

    Update pellets.js

--- public/javascript/pellets.js
+++ public/javascript/pellets.js
@@ -1,45 +1,15 @@
 
-//where we create a
-// function spawnpellets(){
-//     var pellet = document.createElement("img");
-//     pellet.setAttribute("src", "public/images/red.png");
-//     pellet.classList.add("pellet")
-//     var board = document.querySelector(".game-page");
-//     var boardheight = board.clientWidth;
-//     var boardwidth = board.clientHeight;
-//     var randomx = randoms()[0];
-//     var randomy = randoms()[1]; 
-//     pellet.style.left = randomx + "px";
-//     pellet.style.top = randomy + "px";
-//     board.append(pellet);
-    
-// }
-
-// var pelletes = [];
-// for (var i = 0; i < 200; i++ ) {
-//     pelletes.push(spawnpellets());
-// }
-//if player is on blob, pop pellet from the array
-// function onplayer(){
-//     var blob = document.getElementById("icon");
-//     var pellet = document.getElementById("public/images/red.png")
-//     for (var i = pelletes.length - 1; i >=0; i--){
-//       pelletes[i].show();
-//       if (blob.style.top == pellet.style.top && blob.style.left == pellet.style.left){
-//        pelletes.splice(i, 1);
-//       }
-//     }
-//   }
-// function pelletposition(){
-//   var pellet = document.getElementsByClassName(".pellet");
-//   var pelletpos = pellet.getBoundingClientRect();
-//   x = pelletpos.left;
-//   y = pelletpos.top;
-// }
-
-// function randoms()
-// {
-//     var randomx = parseInt(Math.random()* window.innerWidth - window.innerWidth/4);
-//     var randomy = parseInt(Math.random()* window.innerHeight - window.innerHeight/2);
-//     return [randomx, randomy];
-// }
+function createpellet(){
+    var board = document.querySelector(".game-page");
+    let pellet = document.createElement("img");
+    pellet.setAttribute("src", "public/images/red.png");
+    pellet.style.position = "absolute";
+    board.appendChild(pellet);
+    randomx = parseInt(Math.random()* window.innerWidth - window.innerWidth/3.5);
+    randomy = parseInt(Math.random()* window.innerHeight - window.innerHeight/1.5);
+    pellet.style.left = randomx + "px";
+    pellet.style.top  = randomy + "px";
+    console.log(randomx, randomy);
+    return pellet;
+  
+  }

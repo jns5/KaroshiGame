e0f2f02e8f0b3786b8e00e56bdca600b268e35b0
AloraTab <59279709+AloraTab@users.noreply.github.com>
2020-11-12 10:24:07 +0400

    Fixed pellet spawning to fit board,

--- public/javascript/pellets.js
+++ public/javascript/pellets.js
@@ -1,6 +1,6 @@
 
 function createpellet(){
-    var board = document.querySelector(".game-page");
+    var board = document.querySelector(".game-board");
     let pellet = document.createElement("img");
     pellet.style.height="20px";
     pellet.style.width="20px";
@@ -8,8 +8,8 @@ function createpellet(){
     pellet.setAttribute("src", "images/red.png");
     pellet.style.position = "absolute";
     board.appendChild(pellet);
-    randomx = randoms()[0];
-    randomy = randoms()[1];
+    randomx = parseInt(Math.random()* 800);
+    randomy = parseInt(Math.random()* 500);
     pellet.style.left = randomx + "px";
     pellet.style.top  = randomy + "px";
     console.log(randomx, randomy);
